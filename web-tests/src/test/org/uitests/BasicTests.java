4872a017c4d5e68b32aeb9b4ec606532840d1d6f
Maria Rodionova <mrodionova@aflt-systems.ru>
2025-10-13 12:42:39 +0300

    initial commit

--- web-tests/src/test/org/uitests/BasicTests.java
+++ web-tests/src/test/org/uitests/BasicTests.java
@@ -1,5 +1,6 @@
 package org.uitests;
 
+import org.openqa.selenium.NoSuchElementException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.junit.jupiter.api.AfterEach;
@@ -10,6 +11,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.utils.ChromeWebDriverFactory;
 
+import java.time.Duration;
+
 public class BasicTests {
     ChromeWebDriverFactory chromeWebDriver = new ChromeWebDriverFactory();
     private static final Logger log = LoggerFactory.getLogger(BasicTests.class);
@@ -18,26 +21,31 @@ public class BasicTests {
     @BeforeAll
     static void setup(){
         ChromeWebDriverFactory.createWebDriver();
-        ChromeWebDriverFactory.startDriver();
-
     }
+
 
     @BeforeEach
     void startDriver(){
         ChromeWebDriverFactory.startDriver();
+
+        chromeWebDriver.getDriver()
+                .manage()
+                .timeouts()
+                .implicitlyWait(Duration.ofSeconds(10));
     }
 
     @AfterEach
     void tearDown(){
-        ChromeWebDriverFactory.quit();
+        chromeWebDriver.quit();
     }
 
 
     @Test
-    public void firstBasicTest(){
+    public void firstBasicTest() throws NoSuchElementException {
         log.info("Navigating to training page");
         chromeWebDriver.navigateToUrl("https://otus.home.kartushin.su/training.html");
-        WebElement title =chromeWebDriver.findElement(By.tagName("h1"));
+
+        WebElement title = chromeWebDriver.findElement(By.tagName("h1"));
         log.info("Found header: {}", title.getText());
     }
 
--- web-tests/src/test/org/utils/ChromeWebDriverFactory.java
+++ web-tests/src/test/org/utils/ChromeWebDriverFactory.java
@@ -1,55 +1,100 @@
 package org.utils;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-public class ChromeWebDriverFactory {
+public class ChromeWebDriverFactory implements BrowserActions {
     private static WebDriver driver;
+    private static boolean isInitialized = false;
 
     public static void createWebDriver() {
-        WebDriverManager.chromedriver().setup();
+        if (driver != null) {
+            return;
+        }
+        if (!isInitialized) {
+            WebDriverManager.chromedriver().setup();
+            isInitialized = true;
+        }
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-maximized");
         driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
     }
 
-    public static void startDriver(){
-        driver = new ChromeDriver();
+    public static void startDriver() {
+        if (driver == null) {
+            createWebDriver();
+        }
     }
 
+    @Override
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    @Override
     public void navigateToUrl(String url) {
         if (driver != null) {
             driver.get(url);
         }
     }
 
+    @Override
     public WebElement findElement(By locator) {
         return driver.findElement(locator);
     }
 
-    public static void quit() {
+    @Override
+    public void clickToElement(By locator) {
+        waitForVisible(locator, Duration.ofSeconds(10));
+        driver.findElement(locator).click();
+    }
+
+    @Override
+    public void typeText(By locator, String text) {
+        waitForVisible(locator, Duration.ofSeconds(10));
+        WebElement element = driver.findElement(locator);
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    @Override
+    public String getText(By locator) {
+        waitForVisible(locator, Duration.ofSeconds(10));
+        return driver.findElement(locator).getText();
+    }
+
+    @Override
+    public void waitForVisible(By locator, Duration timeout) {
+        new WebDriverWait(driver, timeout)
+                .until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public void quit() {
         if (driver != null) {
             driver.quit();
             driver = null;
         }
     }
 
-    public void setCookies(String name, String value){
+    public void setCookies(String name, String value) {
         driver.manage().addCookie(new Cookie(name, value));
     }
 
-    public void setTimeouts(int seconds){
+    public void setTimeouts(int seconds) {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(seconds));
     }
 
-public void windowMaximize(){
+    public void windowMaximize() {
         driver.manage().window().maximize();
-}
+    }
 }
